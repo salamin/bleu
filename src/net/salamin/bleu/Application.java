34317212b2bf42d61b018185c2b73fe97117d404
Florian <florian@salamin.net>
2020-11-20 23:25:58 +0100

    French msg

--- src/net/salamin/bleu/Application.java
+++ src/net/salamin/bleu/Application.java
@@ -2,6 +2,6 @@ package net.salamin.bleu;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Bonjour Le Monde!");
     }
 }
